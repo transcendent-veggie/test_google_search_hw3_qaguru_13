1f3d13107e789fac81aad87650ebea728eb16310
Stanislav Yudin <syy.stanislav@gmail.com>
2024-05-21 07:41:53 +0300

    deleted unused lines

--- test_google_search_hw_qaguru_13.py
+++ test_google_search_hw_qaguru_13.py
@@ -1,21 +1,5 @@
 import pytest
 from selene import browser, be, have
-from selenium.webdriver import Chrome, ChromeOptions
-
-'''
-@pytest.fixture()
-def browser_open():
-    browser.open('https://www.google.com/')
-    # print('\nbrowser opened')
-    yield print('\nbrowser closed')
-
-
-def test_search(browser_open):
-    # browser.open('https://google.com')
-    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
-
-'''
 
 
 @pytest.fixture()
